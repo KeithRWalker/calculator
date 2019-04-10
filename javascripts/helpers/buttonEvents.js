4354d8632f9107dddf4aff54ab99afe62e6c2133
Keith Walker <Keithrwalker418@gmail.com>
2019-04-09 21:47:01 -0500

    Calculator Functionality Added

--- javascripts/helpers/buttonEvents.js
+++ javascripts/helpers/buttonEvents.js
@@ -1,27 +1,29 @@
 import calc from '../components/calculator.js';
 
-const divideButton = document.getElementById('divideButton');
+//BUTTON SELECTORS
+const divideButton   = document.getElementById('divideButton');
 const multiplyButton = document.getElementById('multiplyButton');
 const subtractButton = document.getElementById('subtractButton');
-const addButton = document.getElementById('addButton');
-const equalsButton = document.getElementById('equalsButton');
-const oneButton = document.getElementById('oneButton');
-const twoButton = document.getElementById('twoButton');
-const threeButton = document.getElementById('threeButton');
-const fourButton = document.getElementById('fourButton');
-const fiveButton = document.getElementById('fiveButton');
-const sixButton = document.getElementById('sixButton');
-const sevenButton = document.getElementById('sevenButton');
-const eightButton = document.getElementById('eightButton');
-const nineButton = document.getElementById('nineButton');
-const zeroButton = document.getElementById('zeroButton');
+const addButton      = document.getElementById('addButton');
+const equalsButton   = document.getElementById('equalsButton');
+const oneButton      = document.getElementById('oneButton');
+const twoButton      = document.getElementById('twoButton');
+const threeButton    = document.getElementById('threeButton');
+const fourButton     = document.getElementById('fourButton');
+const fiveButton     = document.getElementById('fiveButton');
+const sixButton      = document.getElementById('sixButton');
+const sevenButton    = document.getElementById('sevenButton');
+const eightButton    = document.getElementById('eightButton');
+const nineButton     = document.getElementById('nineButton');
+const zeroButton     = document.getElementById('zeroButton');
 
 const addNumber = (e) => {
-  e.preventDefault();
-  calc.addNumber(e.target.innerHTML);
+    e.preventDefault();
+    calc.addNumber(e.target.innerHTML);
 }
 
-const attachEvents = () => {
+const attachEvents = () => {//EVENT LISTENER FUNCTION FOR BUTTONS//
+    
     oneButton.addEventListener('click', addNumber);
     twoButton.addEventListener('click', addNumber);
     threeButton.addEventListener('click', addNumber);
@@ -37,18 +39,22 @@ const attachEvents = () => {
         e.target.className += " selectedButton";
         calc.setMathType('add');
     });
+
     subtractButton.addEventListener('click', (e) => {
         e.target.className += " selectedButton";
         calc.setMathType('subtract');
     });
+
     multiplyButton.addEventListener('click', (e) => {
         e.target.className += " selectedButton";
         calc.setMathType('multiply');
     });
+
     divideButton.addEventListener('click', (e) => {
         e.target.className += " selectedButton";
         calc.setMathType('divide');
     });
+
         equalsButton.addEventListener('click', () => {
         calc.calculate();
     });
--- javascripts/helpers/util.js
+++ javascripts/helpers/util.js
@@ -3,5 +3,5 @@ const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
 };
-  
+
 export default { printToDom };
